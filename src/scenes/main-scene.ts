da0e26a553838a33db7b1251c73d662fee6419f9
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-04-02 00:46:15 +0200

    feat: show notes on screan

--- src/scenes/main-scene.ts
+++ src/scenes/main-scene.ts
@@ -1,12 +1,17 @@
 import { Logo } from '../objects/logo';
+import { HitNote } from '../objects/hitNote';
 import { INote } from '../interfaces/note.interface';
+import { HitPosition } from '../objects/hitPosition';
 
 export class MainScene extends Phaser.Scene {
   logo: Logo;
   keyboard: any;
   notes: INote[];
   hittedNotes: number[] = [];
-  timer: number = 0;
+  notesObject: HitNote[] = [];
+  scrollSpeed: number = 10;
+  hitPosition: number = 100;
+  startTime: number = 0;
 
   constructor() {
     super({ key: 'MainScene' });
@@ -14,6 +19,8 @@ export class MainScene extends Phaser.Scene {
 
   preload(): void {
     this.load.image('logo', '../assets/logo.png');
+    this.load.image('hitNote', '../assets/skin/hitNote.png');
+    this.load.image('hitPosition', '../assets/skin/hitPosition.png');
     this.notes = [
       {
         direction: 'up',
@@ -42,10 +49,25 @@ export class MainScene extends Phaser.Scene {
       up: Phaser.Input.Keyboard.KeyCodes.Z,
       down: Phaser.Input.Keyboard.KeyCodes.FORWARD_SLASH,
     });
+    this.renderNotes();
+    this.startTime = Date.now();
+    const newHitPositionUp = new HitPosition({
+      scene: this,
+      x: this.hitPosition,
+      y: 150,
+    });
+    const newHitPositionDown = new HitPosition({
+      scene: this,
+      x: this.hitPosition,
+      y: 250,
+    });
   }
 
   handleNoteClick(): void {
-    const time = this.timer;
+    const time =
+      Date.now() -
+      this.startTime -
+      (this.game.renderer.width * this.scrollSpeed) / 6;
 
     this.notes.map((note, index) => {
       if (
@@ -73,15 +95,25 @@ export class MainScene extends Phaser.Scene {
     });
   }
 
-  renderNotes(): void {}
-
-  updateTimer(): void {
-    this.timer = this.game.loop.time;
+  renderNotes(): void {
+    this.notes.map((note) => {
+      const newNote = new HitNote({
+        scene: this,
+        x:
+          (note.delay / 1000) * (60 * this.scrollSpeed) +
+          this.hitPosition +
+          this.game.renderer.width,
+        y: note.direction === 'up' ? 150 : 250,
+        texture: 'hitNote',
+      });
+      this.notesObject = [...this.notesObject, newNote];
+    });
   }
 
   update(): void {
-    this.updateTimer();
-    this.renderNotes();
     this.handleNoteClick();
+    this.notesObject.map((note, index) => {
+      note.updatePosition(this.scrollSpeed);
+    });
   }
 }
