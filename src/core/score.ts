634126fdf2a59e6191f271370b0e35872422363c
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-04-03 01:24:53 +0200

    feature/display hitted cound of every type of note on result screan

--- src/core/score.ts
+++ src/core/score.ts
@@ -1,11 +1,14 @@
 import { noteAccuracyConfig } from '../config/noteAccuracyConfig';
-import { ENoteAccuracy } from '../interfaces/noteAccuracy.interface';
+import {
+  ENoteAccuracy,
+  INoteTypeAndCount,
+} from '../interfaces/noteAccuracy.interface';
 
-const calculateCurrentScore = (hittedNotes: any[]): number => {
+const calculateCurrentScore = (hittedNotes: string[]): number => {
   const notesAccuracy = noteAccuracyConfig.accuracy;
   let score: number = 0;
   let count: any = {};
-  hittedNotes.forEach(function (i) {
+  hittedNotes.forEach((i) => {
     count[i] = (count[i] || 0) + 1;
   });
 
@@ -20,4 +23,28 @@ const calculateCurrentScore = (hittedNotes: any[]): number => {
   return score;
 };
 
-export { calculateCurrentScore };
+const getAllTypesAndCoundHittedNotes = (
+  hittedNotes: string[],
+): INoteTypeAndCount[] => {
+  let count: any = {};
+  let i = 0;
+  for (const [key] of Object.entries(noteAccuracyConfig.accuracy)) {
+    if (key !== ENoteAccuracy.None) {
+      count[key] = 0;
+    }
+    i++;
+  }
+  hittedNotes.forEach((i) => {
+    count[i] = (count[i] || 0) + 1;
+  });
+
+  let newCount: any[] = [];
+
+  for (const [key, value] of Object.entries(count)) {
+    newCount = [...newCount, { noteType: key, count: value }];
+  }
+
+  return newCount;
+};
+
+export { calculateCurrentScore, getAllTypesAndCoundHittedNotes };
--- src/scenes/resultScene.ts
+++ src/scenes/resultScene.ts
@@ -1,7 +1,16 @@
 import { Text } from '../objects/text';
+import { calculateOveralAccuracy } from '../core/accuracy';
+import { getHittedNotes } from '../redux/mapResult';
+import {
+  calculateCurrentScore,
+  getAllTypesAndCoundHittedNotes,
+} from '../core/score';
 
 export class ResultScene extends Phaser.Scene {
   text: Text;
+  accuracy: Text;
+  score: Text;
+  notesTypeAndCount: Text[] = [];
 
   constructor() {
     super({ key: 'ResultScene' });
@@ -16,6 +25,27 @@ export class ResultScene extends Phaser.Scene {
       x: 100,
       y: 50,
       text: 'Result',
+    });
+    this.accuracy = new Text({
+      scene: this,
+      x: 100,
+      y: 100,
+      text: `Accuracy: ${calculateOveralAccuracy(getHittedNotes())}`,
+    });
+    this.score = new Text({
+      scene: this,
+      x: 100,
+      y: 150,
+      text: `Score: ${calculateCurrentScore(getHittedNotes())}`,
+    });
+    getAllTypesAndCoundHittedNotes(getHittedNotes()).map((note, index) => {
+      const newNoteAndCount = new Text({
+        scene: this,
+        x: 100,
+        y: 200 + index * 50,
+        text: `${note.noteType}: ${note.count}`,
+      });
+      this.notesTypeAndCount = [...this.notesTypeAndCount, newNoteAndCount];
     });
   }
 }
