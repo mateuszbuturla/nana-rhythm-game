b8c878071e15091af25500fec5884fe538b87636
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-04-03 21:15:28 +0200

    feature(main-menu)/create play button

--- src/scenes/mainMenu.ts
+++ src/scenes/mainMenu.ts
@@ -1,7 +1,9 @@
+import { Text } from '../objects/text';
 import { Logo } from '../objects/logo';
 
 export class MainMenu extends Phaser.Scene {
   logo: Logo;
+  playButton: Text;
 
   constructor() {
     super({ key: 'MainMenu' });
@@ -20,6 +22,16 @@ export class MainMenu extends Phaser.Scene {
       x: width / 2,
       y: height / 3,
       texture: 'logo',
+    });
+    this.playButton = new Text({
+      scene: this,
+      x: width / 2 - 150,
+      y: height / 3 + 150,
+      text: 'Play',
+    });
+    this.playButton.setInteractive();
+    this.playButton.on('pointerdown', () => {
+      this.scene.start('SongSelection');
     });
   }
 }
