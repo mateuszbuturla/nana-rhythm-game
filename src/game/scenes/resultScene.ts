dad9aaed1bee5642203195573a93449a8736542e
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-04-10 21:05:31 +0200

    feature/result panel change design(perfect/good/bad/mis count and gained mark)

--- src/game/scenes/resultScene.ts
+++ src/game/scenes/resultScene.ts
@@ -4,15 +4,26 @@ import { getCombo, getHittedNotes } from '../redux/mapResult';
 import {
   calculateCurrentScore,
   getAllTypesAndCoundHittedNotes,
+  getCountOfHittedNotesFromType,
 } from '../core/score';
+import { ResultLabelValue } from '../objects/ui/resultLabelValue';
+import { ENoteAccuracy } from '../interfaces/noteAccuracy.interface';
+import { noteAccuracyConfig } from '../config/noteAccuracyConfig';
 
 export class ResultScene extends Phaser.Scene {
   text: Text;
-  accuracy: Text;
-  score: Text;
   notesTypeAndCount: Text[] = [];
   comboObject: Text;
   maxComboObject: Text;
+
+  mark: Text;
+
+  score: ResultLabelValue;
+  accuracy: ResultLabelValue;
+  perfect: ResultLabelValue;
+  good: ResultLabelValue;
+  bad: ResultLabelValue;
+  miss: ResultLabelValue;
 
   constructor() {
     super({ key: 'ResultScene' });
@@ -22,44 +33,136 @@ export class ResultScene extends Phaser.Scene {
 
   create(): void {
     this.preload();
-    this.text = new Text({
+    const width = this.sys.game.canvas.width;
+    const height = this.sys.game.canvas.height;
+
+    this.mark = new Text({
       scene: this,
-      x: 100,
-      y: 50,
-      text: 'Result',
+      x: width / 2,
+      y: height / 3,
+      text: 'A',
+      color: 'green',
+      fontSize: '350px',
+      align: 'center',
+      fontFamily: 'mainFontB',
     });
-    this.accuracy = new Text({
+
+    // this.scoreLabel = new Text({
+    //   scene: this,
+    //   x: width / 2 - 300,
+    //   y: height / 3 + 200,
+    //   text: `Score`,
+    //   color: 'white',
+    //   fontSize: '44px',
+    //   align: 'center',
+    // });
+    // this.score = new Text({
+    //   scene: this,
+    //   x: width / 2 - 300,
+    //   y: height / 3 + 300,
+    //   text: `${calculateCurrentScore(getHittedNotes())}`,
+    //   color: 'white',
+    //   fontSize: '95px',
+    //   align: 'center',
+    // });
+
+    this.score = new ResultLabelValue({
       scene: this,
-      x: 100,
-      y: 100,
-      text: `Accuracy: ${calculateOveralAccuracy(getHittedNotes())}%`,
+      x: width / 2 - 300,
+      y: height / 3 + 200,
+      label: 'Score',
+      value: `${calculateCurrentScore(getHittedNotes())}`,
+      color: 'white',
     });
-    this.score = new Text({
+    this.accuracy = new ResultLabelValue({
       scene: this,
-      x: 100,
-      y: 150,
-      text: `Score: ${calculateCurrentScore(getHittedNotes())}`,
+      x: width / 2 + 300,
+      y: height / 3 + 200,
+      label: 'Accuracy',
+      value: `${calculateOveralAccuracy(getHittedNotes())}%`,
+      color: 'white',
     });
-    this.comboObject = new Text({
+
+    this.perfect = new ResultLabelValue({
       scene: this,
-      x: 300,
-      y: 100,
-      text: `combo: ${getCombo().combo}`,
+      x: width / 2 - 600,
+      y: height / 3 + 400,
+      label: 'Perfect',
+      value: `${getCountOfHittedNotesFromType(
+        ENoteAccuracy.Perfect,
+        getHittedNotes(),
+      )}`,
+      color: noteAccuracyConfig.accuracy.Perfect.color,
     });
-    this.maxComboObject = new Text({
+
+    this.good = new ResultLabelValue({
       scene: this,
-      x: 300,
-      y: 150,
-      text: `max combo: ${getCombo().maxCombo}`,
+      x: width / 2 - 300,
+      y: height / 3 + 400,
+      label: 'Good',
+      value: `${getCountOfHittedNotesFromType(
+        ENoteAccuracy.Good,
+        getHittedNotes(),
+      )}`,
+      color: noteAccuracyConfig.accuracy.Good.color,
     });
-    getAllTypesAndCoundHittedNotes(getHittedNotes()).map((note, index) => {
-      const newNoteAndCount = new Text({
-        scene: this,
-        x: 100,
-        y: 200 + index * 50,
-        text: `${note.noteType}: ${note.count}`,
-      });
-      this.notesTypeAndCount = [...this.notesTypeAndCount, newNoteAndCount];
+
+    this.bad = new ResultLabelValue({
+      scene: this,
+      x: width / 2,
+      y: height / 3 + 400,
+      label: 'Bad',
+      value: `${getCountOfHittedNotesFromType(
+        ENoteAccuracy.Bad,
+        getHittedNotes(),
+      )}`,
+      color: noteAccuracyConfig.accuracy.Bad.color,
     });
+
+    this.miss = new ResultLabelValue({
+      scene: this,
+      x: width / 2 + 300,
+      y: height / 3 + 400,
+      label: 'Miss',
+      value: `${getCountOfHittedNotesFromType(
+        ENoteAccuracy.Miss,
+        getHittedNotes(),
+      )}`,
+      color: noteAccuracyConfig.accuracy.Miss.color,
+    });
+
+    // this.text = new Text({
+    //   scene: this,
+    //   x: 100,
+    //   y: 50,
+    //   text: 'Result',
+    // });
+    // this.accuracy = new Text({
+    //   scene: this,
+    //   x: 100,
+    //   y: 100,sdsdsdsd
+    //   text: `Accuracy: ${calculateOveralAccuracy(getHittedNotes())}%`,
+    // });
+    // // this.comboObject = new Text({
+    //   scene: this,
+    //   x: 300,
+    //   y: 100,
+    //   text: `combo: ${getCombo().combo}`,
+    // });
+    // this.maxComboObject = new Text({
+    //   scene: this,
+    //   x: 300,
+    //   y: 150,
+    //   text: `max combo: ${getCombo().maxCombo}`,
+    // });
+    // getAllTypesAndCoundHittedNotes(getHittedNotes()).map((note, index) => {
+    //   const newNoteAndCount = new Text({
+    //     scene: this,
+    //     x: 100,
+    //     y: 200 + index * 50,
+    //     text: `${note.noteType}: ${note.count}`,
+    //   });
+    //   this.notesTypeAndCount = [...this.notesTypeAndCount, newNoteAndCount];
+    // });
   }
 }
