bf6fe7b2a16d6c4c7d458389d550b8e9ac6be739
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-04-14 18:12:48 +0200

    feature/create health bar

--- src/game/core/health/index.ts
+++ src/game/core/health/index.ts
@@ -1,12 +1,14 @@
 import { IHealth } from '../../interfaces/health.interface';
 import { ENoteAccuracy } from '../../interfaces/noteAccuracy.interface';
 import { noteAccuracyConfig } from '../../config/noteAccuracyConfig';
+import { HealthBar } from '../../objects/game/healthBar';
 
 export class Health {
   scene: Phaser.Scene;
   maxHealth: number = 100;
   currentHealth: number = 100;
   healthDrain: number;
+  healthBar: HealthBar;
 
   constructor(aParams: IHealth) {
     this.scene = aParams.scene;
@@ -14,15 +16,29 @@ export class Health {
     this.initHealth();
   }
 
-  initHealth(): void {}
+  initHealth(): void {
+    this.healthBar = new HealthBar({
+      scene: this.scene,
+    });
+  }
 
   decrementHealth(): void {
     this.currentHealth -= this.healthDrain;
+    if (this.currentHealth < 0) {
+      this.currentHealth = 0;
+    }
+    this.healthBar.updateHealthBar((this.currentHealth / this.maxHealth) * 100);
   }
 
   increaseHealth(accuracy: ENoteAccuracy): void {
     this.currentHealth +=
       (this.healthDrain / 4) *
       noteAccuracyConfig.accuracy[accuracy].accuracyValue;
+
+    if (this.currentHealth > this.maxHealth) {
+      this.currentHealth = this.maxHealth;
+    }
+
+    this.healthBar.updateHealthBar((this.currentHealth / this.maxHealth) * 100);
   }
 }
--- src/game/interfaces/health.interface.ts
+++ src/game/interfaces/health.interface.ts
@@ -2,5 +2,8 @@ interface IHealth {
   scene: Phaser.Scene;
   healthDrain: number;
 }
+interface IHealthBar {
+  scene: Phaser.Scene;
+}
 
-export { IHealth };
+export { IHealth, IHealthBar };
