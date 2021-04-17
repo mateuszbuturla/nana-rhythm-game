3d6a61395b3a02d8426573d84f4baf625a0c3d14
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-04-17 14:01:51 +0200

    feature/create container

---
+++ nana.Game/GameObjects/Container/Container.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace nanaGame.GameObjects.Container
+{
+    public class Container : Component
+    {
+        public List<Component> _components;
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            foreach (var component in _components)
+            {
+                component.Draw(gameTime, spriteBatch);
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var component in _components)
+            {
+                component.Update(gameTime);
+            }
+        }
+    }
+}
