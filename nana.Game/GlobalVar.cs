b83f5ed699055dcc01f5a1615cbbfb19a134f105
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-04-18 18:02:20 +0200

    feature/create global variables

---
+++ nana.Game/GlobalVar.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+/// <summary>
+/// Contains global variables for project.
+/// </summary>
+public static class GlobalVar
+{
+    /// <summary>
+    /// Global variable that is constant.
+    /// </summary>
+    public static SpriteBatch SpriteBatch;
+
+    public static GraphicsDeviceManager Graphic;
+
+    public static ContentManager Content;
+}
