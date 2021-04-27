0024bf0122b6411c1c24d7d32db5dc9d3490981e
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-04-27 14:34:01 +0200

    feature/change checkbox state

--- nana.Game/GameObjects/Checkbox/Checkbox.cs
+++ nana.Game/GameObjects/Checkbox/Checkbox.cs
@@ -19,16 +19,28 @@ namespace nanaGame.GameObjects.Checkbox
 
         private NanaUtils utils;
 
-        Component parent;
-
         Texture2D currentTexture;
         Texture2D checkedTexture;
         Texture2D unCheckedTexture;
         SpriteFont font;
 
+        private MouseState _currentMouse;
+        private bool _isHovering;
+        private MouseState _previousMouse;
+
         public Vector2 scale { get; set; }
 
         public Vector2 size;
+
+        public Vector2 checkboxSize;
+
+        public Rectangle Rectangle
+        {
+            get
+            {
+                return new Rectangle((int)realPosition.X, (int)realPosition.Y, (int)checkboxSize.X, (int)checkboxSize.Y);
+            }
+        }
 
         public Checkbox(string label, bool isChecked = false, Component parent = null)
         {
@@ -36,15 +48,18 @@ namespace nanaGame.GameObjects.Checkbox
             this.isChecked = isChecked;
             this.label = label;
             this.parent = parent;
-            font = GlobalVar.Content.Load<SpriteFont>("Font");
+            font = GlobalVar.Content.Load<SpriteFont>("mainFontSmall");
             checkedTexture = utils.LoadTextureFromFile("assets/checkboxChecked.png");
             unCheckedTexture = utils.LoadTextureFromFile("assets/checkbox.png");
             UpdateTexture();
             size = new Vector2(currentTexture.Width / 2, currentTexture.Height / 2);
+            checkboxSize = new Vector2(currentTexture.Width + 25 + font.MeasureString(label).X, currentTexture.Height);
         }
 
         public override void DrawObject(GameTime gameTime)
         {
+            UpdateTexture();
+
             GlobalVar.SpriteBatch.Draw(currentTexture, new Vector2(realPosition.X, realPosition.Y + size.Y / 2), new Rectangle(0, 0, currentTexture.Width, currentTexture.Height), Color.White, 0, size, new Vector2(scale.X, scale.X), SpriteEffects.None, 1);
             GlobalVar.SpriteBatch.DrawString(font, label, new Vector2(realPosition.X + 25, realPosition.Y + 5), Color.White);
         }
@@ -63,6 +78,22 @@ namespace nanaGame.GameObjects.Checkbox
 
         public override void UpdateObject(GameTime gameTime)
         {
+            _previousMouse = _currentMouse;
+            _currentMouse = Mouse.GetState();
+
+            var mouseRectangle = new Rectangle(_currentMouse.X, _currentMouse.Y, 1, 1);
+
+            _isHovering = false;
+
+            if (mouseRectangle.Intersects(Rectangle))
+            {
+                _isHovering = true;
+
+                if (_currentMouse.LeftButton == ButtonState.Released && _previousMouse.LeftButton == ButtonState.Pressed)
+                {
+                    isChecked = !isChecked;
+                }
+            }
         }
     }
 }
